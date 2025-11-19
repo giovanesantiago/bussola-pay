c600951364f531aada60d6c663440756cbb84937
Giovane Santiago <giovanedevweb@gmail.com>
2025-11-19 17:12:29 -0300

    feat: estrutura inicial

---
+++ bussola-pay/src/main/java/br/com/bussolapay/BussolaPayApplication.java
@@ -0,0 +1,13 @@
+package br.com.bussolapay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BussolaPayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BussolaPayApplication.class, args);
+    }
+
+}
---
+++ bussola-pay/src/test/java/br/com/bussolapay/BussolaPayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bussolapay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BussolaPayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
