6cf54778e04a7ae2e354e208be9bc2b91f74f298
Giovane Santiago <giovanedevweb@gmail.com>
2025-11-19 17:12:43 -0300

    feat: tela login

---
+++ bussola-pay/src/main/java/br/com/bussolapay/config/MvcConfig.java
@@ -0,0 +1,17 @@
+package br.com.bussolapay.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/").setViewName("/dashboard");
+        registry.addViewController("/dashboard").setViewName("dashboard");
+    }
+
+}
